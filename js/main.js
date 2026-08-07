c370ac3048537d9075a5fdff1a9b9f0959a77336
2818194829 <2818194829@qq.com>
2026-08-08 03:45:59 +0800

    更新至 v1.8.3：下载链接、版本号与更新日志

--- js/main.js
+++ js/main.js
@@ -139,7 +139,7 @@
           <div class="pg__row">
             <div class="pg__row-icon" style="--c:#0891b2">ZIP</div>
             <div class="pg__row-main">
-              <div class="pg__row-title">source-code-v1.8.2.zip</div>
+              <div class="pg__row-title">source-code-v1.8.3.zip</div>
               <div class="pg__row-sub">github.com · 6.8 MB</div>
               <div class="pg__bar"><i style="width:34%"></i></div>
             </div>
@@ -149,7 +149,7 @@
           <div class="pg__row">
             <div class="pg__row-icon" style="--c:#059669">EXE</div>
             <div class="pg__row-main">
-              <div class="pg__row-title">Neutron-Browser-Setup-1.8.2.exe</div>
+              <div class="pg__row-title">Neutron-Browser-Setup-1.8.3.exe</div>
               <div class="pg__row-sub">github.com · 77.0 MB</div>
               <div class="pg__bar"><i style="width:100%;background:#34d399"></i></div>
             </div>
