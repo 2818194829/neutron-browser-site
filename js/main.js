81bdc2d866a9b8cf98754f7600b8438f4b30ab62
杨俊文 <14710666+yang-jun_wen@user.noreply.gitee.com>
2026-08-22 14:13:38 +0800

    Update to v1.11.5: fix live-skin chrome bar stacking & bookmark contrast

--- js/main.js
+++ js/main.js
@@ -139,7 +139,7 @@
           <div class="pg__row">
             <div class="pg__row-icon" style="--c:#0891b2">ZIP</div>
             <div class="pg__row-main">
-              <div class="pg__row-title">source-code-v1.11.4.zip</div>
+              <div class="pg__row-title">source-code-v1.11.5.zip</div>
               <div class="pg__row-sub">github.com · 6.8 MB</div>
               <div class="pg__bar"><i style="width:34%"></i></div>
             </div>
@@ -149,7 +149,7 @@
           <div class="pg__row">
             <div class="pg__row-icon" style="--c:#059669">EXE</div>
             <div class="pg__row-main">
-              <div class="pg__row-title">Neutron-Browser-Setup-1.11.4.exe</div>
+              <div class="pg__row-title">Neutron-Browser-Setup-1.11.5.exe</div>
               <div class="pg__row-sub">github.com · 100 MB</div>
               <div class="pg__bar"><i style="width:100%;background:#34d399"></i></div>
             </div>
